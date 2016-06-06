63793fccc4e9877838bb1dc270d39c269bd83d43
Brian Hague <bhague1281@gmail.com>
2016-06-06 13:19:24 -0700

    ajax w/json example

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+  // AJAX w/JSON
 
+  // AJAX with a callback
+  // $.get('data.json', function(data) {
+  //   console.log(data);
+  // });
+
+  // AJAX with a done promise
+  // $.get('data.json').done(function(data) {
+  //   console.log(data);
+  // });
+  // console.log('data is here!');
+
+  // Local Storage
 });
