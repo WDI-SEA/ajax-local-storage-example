350161400867d33a69a6a55d82bbe262e84c68ec
Brian Hague <bhague1281@gmail.com>
2016-06-06 13:46:29 -0700

    ajax and local storage examples

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,62 @@
 $(document).ready(function() {
-  // AJAX w/JSON
+  /*
+   * AJAX w/JSON
+   */
 
-  // AJAX with a callback
-  // $.get('data.json', function(data) {
-  //   console.log(data);
-  // });
+  /*
+   * AJAX with a callback
+   */
+  $.get('data.json', function(data) {
+    console.log(data);
+  });
 
-  // AJAX with a done promise
-  // $.get('data.json').done(function(data) {
-  //   console.log(data);
-  // });
-  // console.log('data is here!');
+  /*
+   * AJAX with a done promise
+   */
+  $.get('data.json').done(function(data) {
+    console.log(data);
+  });
+  console.log('data is here!');
 
-  // Local Storage
+  /*
+   * Local Storage
+   */
+
+  /*
+  * Local Storage with a string
+  */
+  localStorage.name = 'Brian';
+  console.log(localStorage.name);
+
+  /*
+  * Local Storage with a number
+  */
+  localStorage.highScore = 42;
+  console.log(localStorage.highScore);
+
+
+  /*
+  * Local Storage with an object
+  */
+  var person = {
+    name: 'Brian',
+    location: 'Seattle',
+    occupation: 'manager',
+    printFullName: function() {
+      console.log(this.name);
+    }
+  };
+
+  // storing the object to local storage
+  localStorage.person = JSON.stringify(person);
+  console.log(localStorage.person);
+
+  // Try to parse the JSON, and handle a case where an error occurs
+  try {
+    console.log(JSON.parse(localStorage.person));
+  } catch (error) {
+    console.log(error);
+    console.log('something went wrong');
+  }
+
 });
